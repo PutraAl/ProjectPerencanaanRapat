d53fbbc12f1a5532305cead557fee6c4b1d8d0e8
Putra Al <palamsyah55@gmail.com>
2025-10-07 21:03:14 +0700

    Update geng

---
+++ page/index.php
@@ -0,0 +1,60 @@
+<?php 
+include "../connection/server.php"
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pengelolaan Rapat Politenik Negeri Batam</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg fixed-top bg-light navbar-light">
+  <div class="container">
+    <a class="navbar-brand" href="#"><img id="MDB-logo"
+        src="../assets/img/Polibatam.png" alt="MDB Logo"
+        draggable="false" height="40" /></a>
+    <button class="navbar-toggler" type="button" data-mdb-collapse-init data-mdb-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <i class="fas fa-bars"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto  align-items-center">
+        <li class="nav-item">
+          <a class="nav-link mx-2" href="#!"><i class="fas fa-bell pe-2"></i>Profile</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link mx-2" href="#!"><i class="fas fa-heart pe-2"></i>Total Rapat</a>
+        </li>
+
+    
+        <li class="nav-item ms-3">
+          <a class="btn btn-dark btn-rounded" href="#!">Sign in</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- Navbar -->
+
+<!-- Website Utama -->
+<div class="row row-cols-12" style="margin-top: 125px;">
+  <div class="col-6">
+  <h2 class="text-center">Pengelolaan Rapat</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nulla reiciendis accusantium delectus laborum molestias dignissimos eius eveniet minus officiis sapiente, voluptate quidem iusto aliquam amet exercitationem commodi. Provident labore neque ut adipisci totam, voluptatum tempora ex rem quidem quos qui minus, voluptas reprehenderit, molestias corporis. Cupiditate ullam nisi perspiciatis laborum nostrum iste?
+  <br>
+  Maxime accusantium laboriosam iste nam sint est ea sequi impedit molestiae consectetur temporibus numquam ut, dolorum reprehenderit vero doloribus. Sed sequi nobis nam neque. Necessitatibus cum, suscipit esse tenetur omnis veritatis optio eveniet illo natus atque. Sint natus nulla non ducimus perspiciatis dolores ab cupiditate fuga dolorum.</p>
+  </div>
+  <div class="col-6">
+  gasad
+  </div>
+</div>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</html>
