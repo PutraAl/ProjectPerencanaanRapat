1a50cb263d096553be4ba0734fade8841f8d1ffe
Putra Al <palamsyah55@gmail.com>
2025-10-19 21:10:16 +0700

    Update frontend

--- page/index.php
+++ page/index.php
@@ -23,7 +23,7 @@ include "../connection/server.php"
 <body>
 
   <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg justify-content-between fixed-top bg-light navbar-light">
+  <nav class="navbar py-4 navbar-expand-lg justify-content-between fixed-top bg-light navbar-light">
     <div class="container">
       <a class="navbar-brand" href="#"><img id="MDB-logo" src="../assets/img/Polibatam.png" alt="MDB Logo"
           draggable="false" height="40" /></a>
@@ -66,16 +66,21 @@ include "../connection/server.php"
   <!-- Website Utama -->
   <div class="row row-cols-12 " style="margin-top: 125px;">
     <div class="col-12 col-md-6">
-      <h2 class="text-center">Pengelolaan Rapat</h2>
-      <p class="px-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nulla reiciendis accusantium
-        delectus laborum molestias dignissimos eius eveniet minus officiis sapiente, voluptate quidem iusto aliquam amet
-        exercitationem commodi. Provident labore neque ut adipisci totam, voluptatum tempora ex rem quidem quos qui
-        minus, voluptas reprehenderit, molestias corporis. Cupiditate ullam nisi perspiciatis laborum nostrum iste?
+      <h2 class="text-center py-2 overflow-hidden">Pengelolaan Rapat</h2>
+      <p class="px-4 "">
+       Politeknik Negeri Batam (Polibatam) terletak di Pulau Batam, kawasan perdagangan dan pelabuhan
+        bebas yang strategis di perbatasan perairan internasional. Berdiri sejak tahun 2000 berdasarkan Akta Pendirian
+        Notaris Soehendro Gautama, SH, Polibatam terus berkembang sebagai institusi pendidikan vokasi unggulan yang
+        menghasilkan lulusan siap kerja dan berdaya saing global.
         <br>
-        Maxime accusantium laboriosam iste nam sint est ea sequi impedit molestiae consectetur temporibus numquam ut,
-        dolorum reprehenderit vero doloribus. Sed sequi nobis nam neque. Necessitatibus cum, suscipit esse tenetur omnis
-        veritatis optio eveniet illo natus atque. Sint natus nulla non ducimus perspiciatis dolores ab cupiditate fuga
-        dolorum.</p>
+        Dalam mendukung kegiatan akademik dan organisasi kemahasiswaan yang dinamis, Polibatam memerlukan sistem yang
+        efisien untuk mengatur berbagai rapat dan koordinasi antar pihak. Oleh karena itu, Website Pengelolaan Rapat
+        Mahasiswa Polibatam hadir sebagai solusi digital untuk membantu proses perencanaan, penjadwalan, pencatatan, dan
+        dokumentasi rapat agar lebih terorganisir, transparan, dan mudah diakses oleh seluruh civitas akademika.
+        <br>
+        Melalui sistem ini, setiap kegiatan rapat — baik internal organisasi, program studi, maupun antar lembaga —
+        dapat dikelola secara terstruktur, sehingga mendukung semangat profesionalisme dan efisiensi di lingkungan
+        Politeknik Negeri Batam.</p>
     </div>
     <div class="col-12 col-md-6 justify-content-center align-content-center align-items-center">
       <img class="mx-auto d-block" src="../assets/img/Polibatam.png" height="250px" width="250px" alt="">
@@ -85,10 +90,10 @@ include "../connection/server.php"
   <hr>
 
   <div class="image-wrap d-flex justify-content-around py-4">
-    <img src="../assets/img/Polibatam.png" alt="" width="100px" height="100px" srcset="">
-    <img src="../assets/img/Polibatam.png" alt="" width="100px" height="100px" srcset="">
-    <img src="../assets/img/Polibatam.png" alt="" width="100px" height="100px" srcset="">
-    <img src="../assets/img/Polibatam.png" alt="" width="100px" height="100px" srcset="">
+    <img src="../assets/img/pol1.png" alt=""  srcset="">
+    <img src="../assets/img/pol2.png" alt=""  srcset="">
+    <img src="../assets/img/pol3.png" alt=""  srcset="">
+    <img src="../assets/img/pol4.png" alt=""  srcset="">
   </div>
 
   <hr>
@@ -97,45 +102,37 @@ include "../connection/server.php"
 
 
     <h1 class="text-center my-4">Tujuan Website</h1>
-    <div class="row justify-content-center g-4">
+    <div class="row  justify-content-center g-4">
 
 
-      <div class="col-12 col-md-4">
+      <div class="col-12 py-4 col-md-4  px-5 px-md-3">
         <div class="card text-center">
           <div class="card-body">
             <img src="../assets/img/Polibatam.png" class="card-img-top img-fluid" alt="...">
 
-            <h5 class="card-title">Card 1</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s
+            <h5 class="card-title">Meningkatkan Efisiensi dan Ketepatan Waktu Rapat</h5>
+            <p class="card-text text-center">Website ini bertujuan untuk membantu mahasiswa dan pihak kampus dalam menjadwalkan serta mengelola rapat dengan lebih efisien. Melalui sistem digital, proses pemesanan waktu, pengiriman undangan, dan pengingat rapat dapat dilakukan secara otomatis sehingga meminimalkan keterlambatan dan bentrok jadwal.s
               content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-
           </div>
         </div>
       </div>
-      <div class="col-12 col-md-4">
+      <div class="col-12 py-4 col-md-4 px-5 px-md-3">
         <div class="card text-center">
           <div class="card-body">
             <img src="../assets/img/Polibatam.png" class="card-img-top img-fluid" alt="...">
-
-            <h5 class="card-title">Card 1</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s
-              content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-
+            <h5 class="card-title">Mempermudah Mengelola Data Rapat</h5>
+            <p class="card-text text-center">Sistem Pengelolaan Rapat ini berguna untuk mempermudah panitia untuk mengelola data-data rapat
+              </p>
           </div>
         </div>
       </div>
-      <div class="col-12 col-md-4">
+      <div class="col-12 py-4 col-md-4 px-5 px-md-3">
         <div class="card text-center">
           <div class="card-body">
             <img src="../assets/img/Polibatam.png" class="card-img-top img-fluid" alt="...">
 
-            <h5 class="card-title">Card 1</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s
-              content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-
+            <h5 class="card-title">Meningkatkan Koordinasi dan Transparansi antar Pihak</h5>
+            <p class="card-text text-center">Website ini memfasilitasi komunikasi dan koordinasi antara mahasiswa, dosen pembimbing, dan pihak organisasi kampus dalam menyelenggarakan rapat. Informasi rapat dapat diakses secara terbuka oleh pihak yang berwenang, sehingga mendukung transparansi dan akuntabilitas kegiatan organisasi di lingkungan Politeknik Negeri Batam.</p>
           </div>
         </div>
       </div>
@@ -148,38 +145,61 @@ include "../connection/server.php"
   <hr>
 
 
-  <div class="contact">
+  <div class="contact" id="contact">
 
     <h1 class="text-center">Contact Us</h1>
-    <p>Lapor Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet est fugit quod, libero autem ullam
-      recusandae quasi laborum natus velit corporis! Vitae iure suscipit impedit voluptatum architecto? Corrupti unde,
-      molestias placeat, eum odit delectus, nisi soluta officia voluptates esse asperiores accusamus ratione quis
-      voluptatum quam excepturi ab sequi in animi.</p>
+    <p class="text-center">Ada masalah? isi data dibawah ini, semua nya akan kami jawab.</p>
 
 
     <form>
       <div class="wrap-contact">
 
         <div class="mb-3 mt-4 text-center">
-          <label for="exampleInputEmail1" class="form-label fw-bold fs-3">Email</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan Email Anda" required>
-          
+          <label for="exampleInputEmail1" class="form-label fw-bold fs-4">Email</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+            placeholder="Masukkan Email Anda" required>
+
         </div>
         <div class="mb-3 text-center">
-          <label for="nama" class="form-label fw-bold fs-3">Nama</label>
+          <label for="nama" class="form-label fw-bold fs-4">Nama</label>
           <input type="text" class="form-control" id="nama" placeholder="Masukkan Nama Anda" required>
         </div>
         <div class="mb-3 text-center">
-          <label for="nama" class="form-label fw-bold fs-3">Keluhan</label>
+          <label for="nama" class="form-label fw-bold fs-4">Keluhan</label>
           <textarea name="" id="" class="form-control" placeholder="Apa keluhan anda?" rows="5"></textarea>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary w-100">Submit</button>
       </div>
-      </form>
+    </form>
   </div>
 
 
+<footer class="footer my-5">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <div class="footer-brand px-5">
+                  <img src="../assets/img/Polibatam.png" alt="">
+                </div>
+             
+            </div>
 
+            <div class="col-md-6 text-md-end">
+                <ul class="footer-links">
+                  <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Terms</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="copyright text-center">
+            © PBL Group 2
+        </div>
+    </div>
+</footer>
+  
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
   integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
