57807ed058a6b68ecaf97b9bf3745f31655e60d0
KQuinn30 <yodapratama30@gmail.com>
2025-11-23 04:00:25 +0700

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="../assets/css/rapat.css">
+    <link rel="stylesheet" href="../assets/css/admindashboard.css">
 </head>
 <body>
     <div class="container">
