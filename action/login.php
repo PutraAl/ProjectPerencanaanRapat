bac50601b0289e29418c578f60cb89c969fd9fbf
Putra Al <palamsyah55@gmail.com>
2025-12-03 21:17:33 +0700

    -

--- action/login.php
+++ action/login.php
@@ -13,6 +13,7 @@ if(isset($_POST['login'])) {
         $_SESSION['username'] = $checkRole['username'];
         $_SESSION['login'] = 'login';
         if($checkRole['role'] == 'admin') {
+
             echo "
             <script>
             alert('Selamat anda berhasil Login sebagai Admin');
@@ -21,6 +22,7 @@ if(isset($_POST['login'])) {
             ";
         }
         elseif($checkRole['role'] == 'peserta') {
+
              echo "
             <script>
             alert('Selamat anda berhasil Login sebagai Peserta');
@@ -38,4 +40,3 @@ if(isset($_POST['login'])) {
             ";
         }
 }
-?>
